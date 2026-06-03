cb16087079c56a133e848a95b19d1b649d437896
Charfaoui Yacine <yacineecharfaoui@gmail.com>
2026-06-03 18:48:15 +0100

    CLI version of the weather app

---
+++ Cli_main.js
@@ -0,0 +1,25 @@
+const ps = require("prompt-sync")
+const input = ps()
+const api_key = "8c86b2138c00447ead4102606261004"
+
+async function get_weather(name) {
+    try{
+        const response = await fetch("https://api.weatherapi.com/v1/current.json?key=" + api_key + "&q="+name)
+        if (response.ok) {
+            let data = await response.json()
+            console.log("The Weather in: " + data.location.name + "\n" + "name: " + data.location.name + ' / ' + data.location.country + "\n"
+                + "local time: " + data.location.localtime + "\n" + "Temp_c: " + data.current.temp_c + "\n"
+                + "Temp_f: " + data.current.temp_f + "\n" + "Weather: " + data.current.condition.text + "\n"
+            )
+        } else {
+            console.log("Error " + response.status)
+        }
+    }catch (error){
+        console.log("Something went wrong :"+ error.name)
+    }
+}
+async function main(){
+    let location = input("Enter the city to see the weather: ")
+    await get_weather(location)
+}
+main()
